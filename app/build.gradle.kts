8794550091eba49b76865e85a5ece4fbd4095399
NyaNyagulugulu <support@cnmsb.xin>
2026-03-25 17:04:18 +0800

    miniSDK21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.neko.music"
-        minSdk = 26
+        minSdk = 21
         targetSdk = 36
         versionCode = 40
         versionName = "20260321"
